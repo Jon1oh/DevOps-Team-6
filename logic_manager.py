c8fd650a78881bdef029bb78a74f74962797c91e
Jon Loh <jon.loh@hotmail.com>
2026-09-22 23:27:23 +0800

    creates check_ai_output(), calculate_risk_level() and validate_risk_level_from_ai() functions

---
+++ logic_manager.py
@@ -0,0 +1,58 @@
+# Ensure the AI output contains all required fields and fields are not empty. If any field or value is missing or invalid, return False. Otherwise, return True.
+def check_ai_output(ai_output):
+    required_fields = [
+        "timestamp", # str
+        "phone_number", # str
+        "message_content", # str
+        "scam_probability (%)", # int
+        "risk_level", # str 
+        "scam_type", # str
+        "indicators", # list
+        "explanation", # str
+        "recommendation" # str
+    ]
+    
+    # Ensure all required fields are present in the AI output. Else, return False
+    for field in required_fields:
+        if field not in ai_output:
+            return False
+        
+    # Ensure all fields are not empty
+    for field in required_fields:
+        value = ai_output[field]        
+        if (type(value) is str) and (value is ""): # if the string value is empty
+            return False
+        elif (type(value) is list) and (value is []): # if the list value is empty
+            return False
+        elif value is None:
+            return False
+                
+    # Ensure scam_probability is a number between 0 and 100. Else, return False
+    if not 0 <= ai_output["scam_probability (%)"] <= 100:
+        return False
+    
+    # Ensure risk_level is one of the expected values. Else, return False
+    if ai_output["risk_level"] not in ["Low", "Medium", "High"]:
+        return False
+
+    return True
+
+# Determine risk_level value based on scam_probability number
+# ? Should risk_level be determined by the AI API? (fully dependent on AI)
+# * I feel calculate_risk_level in the logic manager is fine because it can validate the risk_level value returned from the AI API in case it is wrong.
+def calculate_risk_level(probability):
+    if probability >= 80:
+        return "High"
+    elif probability >= 50:
+        return "Medium"
+    else:
+        return "Low"
+
+# Compare AI's risk level of message against our own logic of calculating risk level. In case AI gives inconsistent risk levels.
+def validate_risk_level_from_ai(ai_output):
+    expected = calculate_risk_level(ai_output["scam_probability (%)"])
+    
+    if ai_output["risk_level"] != expected:
+        ai_output["risk_level"] = expected
+        
+    return ai_output
