4078f4242ddb31f2923829e5d1a8c8e6589d4cec
Amir <2601448@sit.singaporetech.edu.sg>
2026-09-24 00:01:05 +0800

    added queryAI function

--- io_manager.py
+++ io_manager.py
@@ -1,5 +1,5 @@
 # this file contains the logic regarding user inputs and the main while loop
-   
+
 #Prints the main menu options
 def mainMenu_options():
     print("1. Check scam message")
@@ -19,11 +19,41 @@ def mainMenu_Input():
         except ValueError:
             print("Incorrect input try again.")
             
-
+#function to query 
+def queryAI_Input():
+    while True:
+        messageContents = input("Paste message content or quit:\n")
+        sourceContents = ""
+        if messageContents != "" and messageContents.isdigit() == False:
+            if messageContents.lower() == 'quit':
+                return messageContents
+            sourceContents = input("Source of the message, quit or back: ")
+            if sourceContents.lower() == "quit":
+                return sourceContents
+            elif(sourceContents.lower() != "back"):
+                return {"messageContents":messageContents, "sourceContents":sourceContents}
+        else:
+            print("Do not leave the message contents blank or just a number")
         
 #main loop of program
 while True:
     mainMenu_options()
     mainMenu_choice = mainMenu_Input()
-
+    match mainMenu_choice:
+        case 3:
+            print("Exiting Program")
+            break
+        case 2:
+            #call the historical analysis function
+            print("case 2")
+        case 1:
+            queryOutput =  queryAI_Input()
+            if queryOutput != "quit":
+                print("query ai now")
+            print(queryOutput)
+            #call the AI_MANAGER
             
+        
+    
+
+    
