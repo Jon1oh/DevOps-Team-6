5a41f149defb4971a6fe6c8c92aa705d9ab96e82
Fahim <2603052@sit.singaporetech.edu.sg>
2026-09-24 23:27:40 +0800

    Create test_data_manager.py and function to load all records

---
+++ data_manager.py
@@ -0,0 +1,20 @@
+#Read and write json format data
+import json
+
+DATA_FILE = "scam_data.json"
+
+def load_records():
+    try:
+        with open(DATA_FILE, "r", encoding="utf-8") as file:
+            print("file found")
+            return json.load(file)
+
+    except FileNotFoundError:
+        print("file not found")
+        return []
+
+    except json.JSONDecodeError:
+        print("decode error")
+        return []
+
+
