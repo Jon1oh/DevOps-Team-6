c59dedb835caba230d92342e6bac04449d3262af
fahim <67324485+fahimmohamed@users.noreply.github.com>
2026-09-25 18:09:28 +0800

    Trying to test commit

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #Read and write json format data
 #Test
 import json
@@ -20,3 +21,27 @@ def load_records():
 
 
 
+=======
+#Read and write json format data
+#Test
+import json
+
+DATA_FILE = "scam_data.json"
+
+def load_records():
+    try:
+        with open(DATA_FILE, "r", encoding="utf-8") as file:
+            print("file found")
+            return json.load(file)
+
+    except FileNotFoundError:
+        print("file not found")
+        return []
+
+    except json.JSONDecodeError:
+        print("decode error")
+        return []
+
+
+
+>>>>>>> fae7ccef73bff7f03977543871fe6bb6ff088694
--- test_data_manager.py
+++ test_data_manager.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from data_manager import load_records
 
 scam_records = load_records()
@@ -18,4 +19,26 @@ for record in scam_records:
     print(f"Message          : {record['message_content']}")
     print(f"Indicators       : {', '.join(record['indicators'])}")
     print(f"Explanation      : {record['explanation']}")
+=======
+from data_manager import load_records
+
+scam_records = load_records()
+
+print("Number of records:", len(scam_records))
+print("First record:", scam_records[0])
+
+for record in scam_records:
+    print("\n========================================")
+    print(f"          SCAM INCIDENT RECORD {record['message_id']} ")
+    print("========================================")
+    print(f"Timestamp        : {record['time_stamp']}")
+    print(f"Phone Number     : {record['phone_number']}")
+    print(f"Country Code     : {record['country_code']}")
+    print(f"Risk Level       : {record['risk_level']}")
+    print(f"Scam Probability : {record['scam_probability (%)']}%")
+    print(f"Scam Type        : {record['scam_type']}")
+    print(f"Message          : {record['message_content']}")
+    print(f"Indicators       : {', '.join(record['indicators'])}")
+    print(f"Explanation      : {record['explanation']}")
+>>>>>>> fae7ccef73bff7f03977543871fe6bb6ff088694
     print(f"Recommendation   : {record['recommendation']}")
